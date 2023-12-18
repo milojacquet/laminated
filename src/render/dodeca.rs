c7479b8c673314495341cab7fb5351d475cafa85
unknown <milojacquet@gmail.com>
2023-12-18 02:33:51 -0800

    Laminated pentultimate

--- src/render/dodeca.rs
+++ src/render/dodeca.rs
@@ -1,3 +1,4 @@
+use crate::enum_iter;
 use crate::preferences::ConcretePuzzlePreferences;
 use crate::preferences::Preferences;
 use crate::puzzle::common::Basis;
@@ -11,10 +12,49 @@ use std::f32::consts::PI;
 
 use crate::util::{color, Mat3, Mat4, Vec3};
 use cgmath::{InnerSpace, Rad};
-use enum_map::Enum;
 
 const SQ5: f32 = 2.2360679774997896964;
 const PHI: f32 = 1.6180339887498948482; //0.5 * (1.0 + 5.0_f32.sqrt());
+
+impl DodecaRay {
+    /// Maps self from conjugate to BinaryConjugate::Id.
+    /// This is an abstract inverse of ray_to_vec.
+    fn unconjugate(&self, conjugate: <Self as ConcreteRaySystem>::Conjugate) -> Self {
+        match conjugate {
+            BinaryConjugate::Id => *self,
+            BinaryConjugate::Conj => {
+                // in the conjugate, (X, ±₁, ±₂) -> -±₁Y + φ ±₂Z -> -±₁Z + φ ±₂Y
+                //                   (Y, ±₁, ±₂) -> -±₁Z + φ ±₂X -> -±₁Y + φ ±₂X
+                //                   (Z, ±₁, ±₂) -> -±₁X + φ ±₂Y -> -±₁X + φ ±₂Z
+                let basis = match self.0 {
+                    Basis::X => Basis::X,
+                    Basis::Y => Basis::Z,
+                    Basis::Z => Basis::Y,
+                };
+                DodecaRay(basis, -self.2, self.1)
+            }
+        }
+    }
+
+    /// Maps self from conjugate to BinaryConjugate::Id.
+    /// This is an abstract version of ray_to_vec.
+    fn conjugate(&self, conjugate: <Self as ConcreteRaySystem>::Conjugate) -> Self {
+        match conjugate {
+            BinaryConjugate::Id => *self,
+            BinaryConjugate::Conj => {
+                // in the conjugate, (X, ±₁, ±₂) -> -±₁Y + φ ±₂Z -> -±₁Z + φ ±₂Y
+                //                   (Y, ±₁, ±₂) -> -±₁Z + φ ±₂X -> -±₁Y + φ ±₂X
+                //                   (Z, ±₁, ±₂) -> -±₁X + φ ±₂Y -> -±₁X + φ ±₂Z
+                let basis = match self.0 {
+                    Basis::X => Basis::X,
+                    Basis::Y => Basis::Z,
+                    Basis::Z => Basis::Y,
+                };
+                DodecaRay(basis, self.2, -self.1)
+            }
+        }
+    }
+}
 
 impl ConcreteRaySystem for DodecaRay {
     type Conjugate = BinaryConjugate;
@@ -22,7 +62,7 @@ impl ConcreteRaySystem for DodecaRay {
     fn axis_to_transform((ray, order): (Self, i8), conjugate: Self::Conjugate) -> Mat4 {
         let multiplier = match conjugate {
             BinaryConjugate::Id => 1.0,
-            BinaryConjugate::Conj => 2.0,
+            BinaryConjugate::Conj => -2.0,
         };
         Mat4::from_axis_angle(
             ray.axis_to_vec(conjugate),
@@ -31,19 +71,18 @@ impl ConcreteRaySystem for DodecaRay {
     }
 
     fn ray_to_vec(&self, conjugate: Self::Conjugate) -> Vec3 {
-        // in the conjugate, (X, ±₁, ±₂) -> - ±₁a⁺ + φ ±₂a⁺⁺
-        // however, we have to rotate 90° around an axis for the rays to line up
-        // this will make puzzle construction easier
+        // in the conjugate, (a, ±₁, ±₂) -> - ±₁a⁺ + φ ±₂a⁺⁺
+        // however, we will swap Y and Z to make puzzle construction easier
 
         match conjugate {
             BinaryConjugate::Id => (PHI * (self.0 + BasisDiff::D1).to_vec() * self.1.to_f32()
                 + (self.0 + BasisDiff::D2).to_vec() * self.2.to_f32())
             .normalize(),
             BinaryConjugate::Conj => {
-                Mat3::from_angle_x(Rad(PI / 2.0))
-                    * (-(self.0 + BasisDiff::D1).to_vec() * self.1.to_f32()
-                        + PHI * (self.0 + BasisDiff::D2).to_vec() * self.2.to_f32())
-                    .normalize()
+                let vec = (-(self.0 + BasisDiff::D1).to_vec() * self.1.to_f32()
+                    + PHI * (self.0 + BasisDiff::D2).to_vec() * self.2.to_f32())
+                .normalize();
+                Vec3::new(vec.x, vec.z, vec.y)
             }
         }
     }
@@ -57,19 +96,20 @@ impl ConcreteRaySystem for DodecaRay {
     }
 }
 
-const SCALE_CIRCUMRAD: f32 = 0.5;
-const SUPER_START: f32 = 0.85;
+const SCALE_CIRCUMRAD: f32 = 0.7;
+const SUPER_START: f32 = 0.7;
 
-/*fn bary(c1: f32, c2: f32, c3: f32, c4: f32, c5: f32) -> Vec3 {
+fn bary(c1: f32, c2: f32, c3: f32, c4: f32, c5: f32) -> Vec3 {
     use crate::puzzle::dodeca::name::PB;
 
     let mat = DodecaRay::axis_to_transform((PB, 1), BinaryConjugate::Id);
     let v1 = Vec3::new((3.0 * SQ5 - 5.0) / 2.0, SQ5, 0.0) * SCALE_CIRCUMRAD;
     let v2 = (mat * v1.extend(1.0)).truncate();
-    let v3 = (mat * v1.extend(1.0)).truncate();
-    let v4 = (mat * v1.extend(1.0)).truncate();
-    let v5 = (mat * v1.extend(1.0)).truncate();
-}*/
+    let v3 = (mat * v2.extend(1.0)).truncate();
+    let v4 = (mat * v3.extend(1.0)).truncate();
+    let v5 = (mat * v4.extend(1.0)).truncate();
+    (c1 * v1 + c2 * v2 + c3 * v3 + c4 * v4 + c5 * v5) / (c1 + c2 + c3 + c4 + c5)
+}
 
 pub fn pentultimate_seeds(_prefs: &ConcretePuzzlePreferences) -> PuzzleSeed<DodecaRay> {
     use crate::puzzle::dodeca::name::*;
@@ -83,55 +123,99 @@ pub fn pentultimate_seeds(_prefs: &ConcretePuzzlePreferences) -> PuzzleSeed<Dode
         HashMap::from([(NUMBER_KEYS[0], vec![-1, 1]), (NUMBER_KEYS[1], vec![1, -1])]),
     ];
 
-    let abstract_viewport = AbstractViewport {
-        x: 0.0,
-        y: 0.0,
-        width: 1.0,
-        height: 1.0,
-    };
+    for conj in enum_iter::<BinaryConjugate>() {
+        let x = match conj {
+            BinaryConjugate::Id => 0.0,
+            BinaryConjugate::Conj => 1.0,
+        };
 
-    let mut stickers: Vec<StickerSeed<DodecaRay>> = vec![];
+        let abstract_viewport = AbstractViewport {
+            x,
+            y: 0.0,
+            width: 1.0,
+            height: 1.0,
+        };
 
-    let v1 = Vec3::new(SQ5 / 2.0, (5.0 + SQ5) / 4.0, (5.0 - SQ5) / 4.0) * SCALE_CIRCUMRAD;
-    let v2 = Vec3::new(0.0, SQ5, 0.0) * SCALE_CIRCUMRAD;
-    let vcen = Vec3::new(0.0, PHI, 1.0) * SCALE_CIRCUMRAD;
+        let mut stickers: Vec<StickerSeed<DodecaRay>> = vec![];
 
-    {
-        let layers: enum_map::EnumMap<DodecaRay, i8> =
-            enum_map! {PB=>1,BL=>1,BR=>1,PL=>1,PR=>1,PD=>1,F=>-1,DR=>-1,DL=>-1,R=>-1,L=>-1,U=>-1};
+        {
+            let layers: enum_map::EnumMap<DodecaRay, i8> =
+                enum_map::EnumMap::from_fn(|ray: DodecaRay| match ray.unconjugate(conj) {
+                    PB => 1,
+                    BL => 1,
+                    BR => 1,
+                    PL => 1,
+                    PR => 1,
+                    PD => 1,
+                    F => -1,
+                    DR => -1,
+                    DL => -1,
+                    R => -1,
+                    L => -1,
+                    U => -1,
+                });
 
-        // center
-        stickers.push(StickerSeed {
-            layers,
-            face: PB,
-            color: PB,
-            vertices: vec![v1, v2, vcen],
+            // center
+            stickers.push(StickerSeed {
+                layers,
+                face: PB.conjugate(conj),
+                color: PB.conjugate(conj),
+                vertices: vec![
+                    bary(SUPER_START, 1.0, 1.0 - SUPER_START, 0.0, 0.0),
+                    bary(1.0 - SUPER_START, 1.0, SUPER_START, 0.0, 0.0),
+                    bary(0.0, SUPER_START, 1.0, 1.0 - SUPER_START, 0.0),
+                    bary(1.0, 1.0, 1.0, 1.0, 1.0),
+                ],
+            });
+            stickers.push(StickerSeed {
+                layers,
+                face: PB.conjugate(conj),
+                color: PL.conjugate(conj),
+                vertices: vec![
+                    bary(0.0, SUPER_START, 1.0, 1.0 - SUPER_START, 0.0),
+                    bary(1.0 - SUPER_START, 1.0, SUPER_START, 0.0, 0.0),
+                    bary(0.0, 1.0, 1.0, 0.0, 0.0),
+                ],
+            });
+        }
+
+        {
+            let layers: enum_map::EnumMap<DodecaRay, i8> =
+                enum_map::EnumMap::from_fn(|ray: DodecaRay| match ray.unconjugate(conj) {
+                    PB => 1,
+                    BL => 1,
+                    DL => 1,
+                    PL => 1,
+                    PR => 1,
+                    PD => 1,
+                    F => -1,
+                    DR => -1,
+                    BR => -1,
+                    R => -1,
+                    L => -1,
+                    U => -1,
+                });
+
+            // corner
+            stickers.push(StickerSeed {
+                layers,
+                face: PB.conjugate(conj),
+                color: PB.conjugate(conj),
+                vertices: vec![
+                    bary(0.0, 1.0, 1.0, 0.0, 0.0),
+                    bary(1.0, 1.0, 0.0, 0.0, 0.0),
+                    bary(0.0, 1.0, 0.0, 0.0, 0.0),
+                ],
+            });
+        }
+
+        viewports.push(ViewportSeed {
+            abstract_viewport,
+            conjugate: conj,
+            stickers,
+            key_layers: key_layers.clone(),
         });
     }
-
-    {
-        let layers: enum_map::EnumMap<DodecaRay, i8> =
-            enum_map! {PB=>1,DR=>1,BR=>1,PL=>1,PR=>1,PD=>1,F=>-1,BL=>-1,DL=>-1,R=>-1,L=>-1,U=>-1};
-
-        // corner
-        stickers.push(StickerSeed {
-            layers,
-            face: PB,
-            color: PB,
-            vertices: vec![
-                Vec3::new((3.0 * SQ5 - 5.0) / 2.0, SQ5, 0.0) * SCALE_CIRCUMRAD,
-                v2,
-                v1,
-            ],
-        });
-    }
-
-    viewports.push(ViewportSeed {
-        abstract_viewport,
-        conjugate: BinaryConjugate::Id,
-        stickers,
-        key_layers: key_layers.clone(),
-    });
 
     PuzzleSeed {
         grips,
